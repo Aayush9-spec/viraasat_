0cf12910dc9602c9bbaa342c24a97d3373d668da
Aayush Kumar Singh <2k24.cs1a.2414142@gmail.com>
2025-09-19 10:23:44 +0530

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,20 +1,24 @@
 import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
- 
-export default async function LocaleLayout({
-  children,
-  params: {locale}
-}: {
-  children: React.ReactNode;
+import {ReactNode} from 'react';
+
+type Props = {
+  children: ReactNode;
   params: {locale: string};
-}) {
-  // Providing all messages to the client
-  // side is the easiest way to get started
-  const messages = await getMessages();
- 
+};
+
+export default async function LocaleLayout({children, params: {locale}}: Props) {
+  // Load translations for the current locale
+  const messages = await getMessages(locale);
+
   return (
-    <NextIntlClientProvider messages={messages}>
-      {children}
-    </NextIntlClientProvider>
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
   );
 }
+
